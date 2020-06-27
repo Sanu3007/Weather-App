97b0a4b129d64219068e5c50fd3d3fe434dd57c6
Sanu <goldie30071999@gmail.com>
2020-06-27 12:49:00 +0530

    Set up for Heroku deployment

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express=require('express')
 const hbs=require('hbs')
 const geocode=require('./utils/geocode')
 const forecast=require('./utils/forecast')
+
 const app=express()
+const port=process.env.PORT || 3000
 
 //Define paths for Express config
 const publicpath=path.join(__dirname,'../public')
@@ -102,6 +104,6 @@ app.get('*',(req,res)=>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('Server started and running....')
 })
